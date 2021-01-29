b8642d4cd00a6915fbe20e2b2c869ea72556f229
Hitarth <hitarthchudgar@gmail.com>
2021-01-29 00:09:52 -0500

    Fetching weather data based of given city

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,18 @@
 //storing the key in a string
 const key = 'S7A2q9xE4lAi1WOUUx9GYxi7HnUyRKTP'; 
 
+//get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+//get city information
 const getCity = async (city) => {
     //base url of api endpoint
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'; 
@@ -15,6 +27,11 @@ const getCity = async (city) => {
 
 }
 
-getCity('toronto')
-    .then(data => console.log(data))
-    .catch(data => console.log(err));
+//all of this returns a promise
+getCity('toronto').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+})
+.catch(err => console.log(err));
+
